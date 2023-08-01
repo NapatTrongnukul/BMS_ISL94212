194067e06305e68e59d77ce5ab8be8245da4bee3
NapatTrongnukul <99158019+NapatTrongnukul@users.noreply.github.com>
2023-08-01 15:57:26 +0700

    Update r_bfe_isl94212.h

--- r_bfe_isl94212.h
+++ r_bfe_isl94212.h
@@ -1,43 +1,6 @@
-/**********************************************************************************************************************
- * DISCLAIMER
- * This software is supplied by Renesas Electronics Corporation and is only intended for use with Renesas products. No
- * other uses are authorized. This software is owned by Renesas Electronics Corporation and is protected under all
- * applicable laws, including copyright laws.
- * THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIES REGARDING
- * THIS SOFTWARE, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. ALL SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED. TO THE MAXIMUM
- * EXTENT PERMITTED NOT PROHIBITED BY LAW, NEITHER RENESAS ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES
- * SHALL BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR ANY REASON RELATED TO
- * THIS SOFTWARE, EVEN IF RENESAS OR ITS AFFILIATES HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
- * Renesas reserves the right, without notice, to make changes to this software and to discontinue the availability of
- * this software. By using this software, you agree to the additional terms and conditions found by accessing the
- * following link:
- * http://www.renesas.com/disclaimer
- *
- * Copyright (C) 2021 Renesas Electronics Corporation. All rights reserved.
- *********************************************************************************************************************/
-/******************************************************************************************************************//**
- * @file          r_bfe_isl94212.h
- * @Version       1.0
- * @brief         Contains macros, data structures and declarations of functions used in r_bfe_isl94212.c
- *********************************************************************************************************************/
-/**********************************************************************************************************************
- * History : DD.MM.YYYY Version  Description
- *         : 16.08.2021 1.00     First Release
- *         : 12.11.2021 1.01     Improved communications timing. Minor bugs are fixed.
- *********************************************************************************************************************/
-
-/*******************************************************************************************************************//**
- * @addtogroup ISL94212
- * @{
- **********************************************************************************************************************/
-
 #ifndef BFE_R_BFE_ISL94212_H_
 #define BFE_R_BFE_ISL94212_H_
 
-/**********************************************************************************************************************
- Includes   <System Includes> , "Project Includes"
- *********************************************************************************************************************/
 /* Generic headers */
 #include <stdint.h>
 #include <stdbool.h>
