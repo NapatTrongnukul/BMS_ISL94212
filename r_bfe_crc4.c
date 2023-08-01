3733384bfe9957087f024c229b5712f7cc2fa967
NapatTrongnukul <99158019+NapatTrongnukul@users.noreply.github.com>
2023-08-01 15:51:29 +0700

    Update r_bfe_crc4.c

--- r_bfe_crc4.c
+++ r_bfe_crc4.c
@@ -1,82 +1,15 @@
-/**********************************************************************************************************************
- * DISCLAIMER
- * This software is supplied by Renesas Electronics Corporation and is only intended for use with Renesas products. No
- * other uses are authorized. This software is owned by Renesas Electronics Corporation and is protected under all
- * applicable laws, including copyright laws.
- * THIS SOFTWARE IS PROVIDED "AS IS" AND RENESAS MAKES NO WARRANTIES REGARDING
- * THIS SOFTWARE, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. ALL SUCH WARRANTIES ARE EXPRESSLY DISCLAIMED. TO THE MAXIMUM
- * EXTENT PERMITTED NOT PROHIBITED BY LAW, NEITHER RENESAS ELECTRONICS CORPORATION NOR ANY OF ITS AFFILIATED COMPANIES
- * SHALL BE LIABLE FOR ANY DIRECT, INDIRECT, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES FOR ANY REASON RELATED TO
- * THIS SOFTWARE, EVEN IF RENESAS OR ITS AFFILIATES HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
- * Renesas reserves the right, without notice, to make changes to this software and to discontinue the availability of
- * this software. By using this software, you agree to the additional terms and conditions found by accessing the
- * following link:
- * http://www.renesas.com/disclaimer
- *
- * Copyright (C) 2021 Renesas Electronics Corporation. All rights reserved.
- *********************************************************************************************************************/
-/******************************************************************************************************************//**
- * @file          r_crc4.c
- * @version       1.00
- * @brief         Contains functions and routines for CRC4 generation and check
- *********************************************************************************************************************/
-/**********************************************************************************************************************
- * History : DD.MM.YYYY Version  Description
- *         : 16.08.2021 1.00     First Release
- *         : 12.11.2021 1.01     Errors in comments are fixed.
- *********************************************************************************************************************/
-
-/**********************************************************************************************************************
- Includes   <System Includes> , "Project Includes"
- *********************************************************************************************************************/
-/* Generic headers */
 #include <stdint.h>
 #include <stdbool.h>
 
-/* bfe related headers */
+
 #include <bfe/r_bfe_crc4.h>
 
-/**********************************************************************************************************************
- Macro definitions
- *********************************************************************************************************************/
-
-/**********************************************************************************************************************
- Local Typedef definitions
- *********************************************************************************************************************/
-
-/**********************************************************************************************************************
- Exported global variables
- *********************************************************************************************************************/
-
-/**********************************************************************************************************************
- Private (static) variables and functions
- *********************************************************************************************************************/
 static e_bms_err_t crc4_calculate (uint8_t *      p_data_array,
                                  uint32_t const length,
                                  uint32_t const offset,
                                  uint8_t *      p_crc4_res);
 
-/* Function Name: crc4_check */
-/******************************************************************************************************************//**
- * This function checks the cheksum in the input data array.
- *
- * This function performs the following tasks:
- * - Parameter checking and processes error conditions.
- * - Calculates the CRC4 of the input data array.
- * - Compares the obtained CRC4 with the last nibble of the input array.
- *
- * @warning     The data array could have any length but more than one byte!
- * @param[in]   p_data_array                A pointer to the data array to be checked.
- * @param[in]   length                      The length (in bytes) of p_data_array.
- * @param[in]   offset                      The data offset (in bytes) of p_data_array.
- * @retval      BFE_SUCCESS                 The CRC4 is correct.
- * @retval      BMS_ERR_CRC_INCORRECT       The CRC in the data array is incorrect.
- * @retval      BMS_ERR_INVALID_POINTER     Input argument is invalid.
- * @retval      BMS_ERR_INVALID_ARGUMENT    Input argument is invalid.
- * @retval      BMS_ERR_BUFFER_SIZE         The data array is not big enough.
- * @retval      BMS_ERR_...                 Inherit from crc4_calculate().
- *********************************************************************************************************************/
+
 e_bms_err_t crc4_check(uint8_t * p_data_array, uint32_t const length, uint32_t const offset)
 {
     e_bms_err_t err = BMS_SUCCESS;    // Error status
@@ -104,27 +37,7 @@ e_bms_err_t crc4_check(uint8_t * p_data_array, uint32_t const length, uint32_t c
 
     return err;
 }
-/**********************************************************************************************************************
- * End of function crc4_check
- *********************************************************************************************************************/
 
-/* Function Name: crc4_add */
-/******************************************************************************************************************//**
- * This function adds CRC4 to the last nibble of the input data array.
- *
- * This function performs the following tasks:
- * - Parameter checking and processes error conditions.
- * - Calculates the CRC4 of the input data array.
- * - Add the obtained CRC4 to the last nibble of the input array.
- *
- * @warning     The data array could have any length but more than one byte!
- * @param[in]   length                      The length (in bytes) of p_data_array.
- * @param[out]  p_data_array                A pointer to the data array to which the checksum will be added.
- * @retval      BFE_SUCCESS                 The CRC4 is successfully added.
- * @retval      BMS_ERR_INVALID_POINTER     Input argument is invalid.
- * @retval      BMS_ERR_BUFFER_SIZE         The data array is not big enough.
- * @retval      BMS_ERR_...                 Inherit from crc4_calculate().
- *********************************************************************************************************************/
 e_bms_err_t crc4_add(uint8_t * p_data_array, uint32_t const length)
 {
     e_bms_err_t err = BMS_SUCCESS;    // Error status
@@ -150,20 +63,7 @@ e_bms_err_t crc4_add(uint8_t * p_data_array, uint32_t const length)
 
     return err;
 }
-/**********************************************************************************************************************
- * End of function crc4_add
- *********************************************************************************************************************/
 
-/**********************************************************************************************************************
- * Function Name: crc4_calculate
- * Description  : This function calculates CRC4 of the input data array using polynomial X^4 + X + 1. The
- * last nibble of the input array is not included into the calculation.
- * Arguments    : p_data_array      A pointer to the data array for CRC calculation;
- *                length            The length (in bytes) of p_data_array;
- *                offset            The data offset (in bytes) of p_data_array;
- *                p_crc4_res        The resultant from the CRC4 calculation.
- * Return Value : BFE_SUCCESS       The CRC4 is successfully calculated.
- *********************************************************************************************************************/
 static e_bms_err_t crc4_calculate (uint8_t *      p_data_array,
                                    uint32_t const length,
                                    uint32_t const offset,
@@ -183,13 +83,11 @@ static e_bms_err_t crc4_calculate (uint8_t *      p_data_array,
 
     bool     carry;
 
-    /* Copy source array so that its content is not compromised. */
     for(uint32_t i = 0; i < length; i++ )
     {
         data_array_cpy[i] = p_data_array[offset + i];
     }
 
-    /* A simple implementation of CRC4 (using polynomial X^4 + X + 1). */
     for(uint32_t i = 0; i < length; i++ )
     {
         /* The last nibble is ignored for CRC4 calculations. */
@@ -228,8 +126,7 @@ static e_bms_err_t crc4_calculate (uint8_t *      p_data_array,
         }
     }
 
-    /* Combine bits to obtain CRC4 result. */
-    *p_crc4_res = 0x00;
+   *p_crc4_res = 0x00;
 
     if(bit0)
     {
@@ -269,6 +166,3 @@ static e_bms_err_t crc4_calculate (uint8_t *      p_data_array,
 
     return BMS_SUCCESS;
 }
-/**********************************************************************************************************************
- * End of function crc4_calculate
- *********************************************************************************************************************/
